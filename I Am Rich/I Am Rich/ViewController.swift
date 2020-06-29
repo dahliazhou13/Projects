6fe3b547b1f0da837b367da6ca7912ebfdb594c4
zhou-ziqi-214746283 <46424591+zhou-ziqi-214746283@users.noreply.github.com>
2020-06-29 12:57:29 -0400

    Created I AM Rich Project

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Doris Zhou on 2020-06-29.
+//  Copyright © 2020 Doris Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
