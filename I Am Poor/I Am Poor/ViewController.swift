ffbf4f6426882964983fb933c5a879674123f9fb
zhou-ziqi-214746283 <46424591+zhou-ziqi-214746283@users.noreply.github.com>
2020-07-05 09:15:10 -0400

    Created two apps

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Doris Zhou on 2020-07-04.
+//  Copyright © 2020 Doris Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
